68972b08c99d584b8f05aa776d45ed391fba761e
William Hobson <williamhobson62@aol.com>
2017-09-23 19:53:35 +0100

    added support for generating a new floppy image

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@ echo "Compiling new binary"
 
 nasm -f bin -o $FILENAME.bin $FILENAME.asm
 
+if [ ! -f "hobos.flp" ]; then
+    echo "Creating blank floppy"
+    dd bs=512 count=2880 if=/dev/zero of=./hobos.flp
+fi
+
 echo "Writing to floppy."
 
 dd status=noxfer conv=notrunc if=hobloader.bin of=hobos.flp
